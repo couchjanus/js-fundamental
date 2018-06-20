8aa4bc66c6b7c7bed3729c958fdbea8ad57f49c0
couch Janus <couchjanus@gmail.com>
2018-06-20 16:31:42 +0300

    {added unit 6

---
+++ js/data.js
@@ -0,0 +1,58 @@
+const data = [
+{
+    id: 0,
+    name: "Cool Cat",
+    price: 177,
+    image: "cat1.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus dignissimos, maxime ea excepturi veritatis itaque."
+},
+{
+    id: 1,
+    name: "Black Cat",
+    price: 666,
+    image: "cat2.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus dignissimos, maxime ea excepturi veritatis itaque."
+},
+{
+    id: 2,
+    name: "Red Cat",
+    price: 555,
+    image: "cat3.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus dignissimos, maxime ea excepturi veritatis itaque."
+},
+{
+    id: 3,
+    name: "Blue Cat",
+    price: 444,
+    image: "cat4.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus dignissimos, maxime ea excepturi veritatis itaque."
+},
+{
+    id: 4,
+    name: "Green Cat",
+    price: 333,
+    image: "cat5.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus dignissimos, maxime ea excepturi veritatis itaque."
+},
+{
+    id: 5,
+    name: "Grey Cat",
+    price: 222,
+    image: "cat6.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus dignissimos, maxime ea excepturi veritatis itaque."
+},
+{
+    id: 6,
+    name: "Orange Cat",
+    price: 777,
+    image: "cat7.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus dignissimos, maxime ea excepturi veritatis itaque."
+},
+{
+    id: 7,
+    name: "Pretty Cat",
+    price: 888,
+    image: "cat8.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus dignissimos, maxime ea excepturi veritatis itaque."
+},
+];
